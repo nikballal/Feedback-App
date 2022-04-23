5d9e1f909057c8df9bc3bc0fc66e3b47dae89583
nikballal <nikhil.ballal1994@gmail.com>
2022-04-23 22:55:16 +1000

    rating select component added

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import Card from "./Shared/Card";
 import Button from "./Shared/Button";
+import RatingSelect from "./RatingSelect";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const [rating, setRating] = useState(10); //'rating' state
   const [btnDisabled, setBtnDisabled] = useState(true); //set character limit by using button disabling
   const [message, setMessage] = useState(""); //set character limit
 
@@ -28,6 +30,11 @@ function FeedbackForm() {
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
+        <RatingSelect
+          select={(rating) => {
+            setRating(rating);
+          }}
+        />
         {/* @todo - rating select component */}
         <div className="input-group">
           <input
@@ -36,10 +43,11 @@ function FeedbackForm() {
             placeholder="Write a review"
             value={text}
           />
-          <Button type="submit" children="Send" isDisabled={btnDisabled} />
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
           {/* 'version' prop becomes primary by default from proptypes, if not mentioned */}
         </div>
-
         {message && <div className="message">{message}</div>}
       </form>
     </Card>
